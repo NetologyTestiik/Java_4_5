888925bb2f2803f78a473b253da7317f7756c053
Neto test <veronikakaravanova60@gmail.com>
2025-08-09 16:22:09 +0500

    change README

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // 500 миль
+        System.out.println(miles);
+    }
+}
